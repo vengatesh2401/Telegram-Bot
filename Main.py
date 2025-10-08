b4427754c9168196553eecc3a00391c4addc4db3
vengatesh2401 <vengatesh3570@gmail.com>
2025-10-08 21:03:21 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,79 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN = """'8423548342:AAHJ4pNJqpVg_gJ0YD4AgNsocKAULZE4W_8'
+BOT_USERNAME: Final ='@AV_AV_Bot'
+"""
+
+#Commands
+async def start_command(_update: Update, _context: ContextTypes.DEFAULT_TYPE):
+    await _update.message.reply_text('Hello! Thanks for chatting with me! I am a AV Bot!')
+
+
+async def help_command(_update: Update, _context: ContextTypes.DEFAULT_TYPE):
+    await _update.message.reply_text('I am a AV Bot! Please type something so I can respond!')
+
+
+async def custom_command(_update: Update, _context: ContextTypes.DEFAULT_TYPE):
+    await _update.message.reply_text('This is a custom command!')
+
+
+# Responses
+
+def handle_response(text: str) -> str:
+    processed: str = text.lower()
+
+    if 'hello' in processed:
+        return 'Hey there!'
+    
+    if 'how are you' in processed:
+        return 'I am good!'
+    
+    if 'i love movie' in processed:
+        return 'Remember to movie name!'
+    
+    return 'I do not understand what you wrote...'
+
+
+async def handle_message(update: Update, _context: ContextTypes.DEFAULT_TYPE):
+    message_type: str  = update.message.chat.type
+    text: str = update.message.text
+
+    print(f'User ({update.message.chat.id}) in {message_type}:"{text}')
+
+    if message_type == 'group':
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, "").strip()
+            response: str = handle_response(new_text)
+        else:
+            return
+    else:
+        response: str = handle_response(text)
+
+    print('Bot:', response)
+    await update.message.reply_text(response)
+
+
+async def error(_update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {_update} caused error {context.error}')
+
+
+if __name__ == '__main__':
+    print('Starting bot...')
+    app = Application.builder().token(TOKEN).build()
+
+    # Commands
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('custom', custom_command))
+
+    #Messages
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    # Errors
+    app.add_error_handler(error)
+    
+    # Poll the bot
+    print('Polling...')
+    app.run_polling(poll_interval=3)
